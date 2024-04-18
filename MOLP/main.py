118a9c4cd5bc0f30ad3da1e379f10765928b481e
Mārtiņš <martins.zemlitis@gmail.com>
2024-04-18 16:01:31 +0300

    readme update

---
+++ MOLP/main.py
@@ -0,0 +1,47 @@
+from MakeMolp import MolpProblem
+
+from SolveOrder import solve_orders
+from SolveEquiv import solve_equiv
+from SolveTnorm import solve_tnorm
+
+import numpy as np
+
+
+# Liels piemērs 
+A_fb = np.array([
+    [2, 3, 2, 4, 1, 3],       
+    [1, 2, 4, 0, 0, 0],          
+    [3, 1, 2, 0, 0, 0],         
+    [4, 2, 3, 5, 3, 4],    
+    [0.2, 0.4, 0.3, 0.5, 0.6, 0.4], 
+    [2, 3, -1, 2, 1, -1],  
+    [4, 2, 3, 0, 0, 0],        
+    [2, 3, 2, 0, 0, 0],           
+    [1, 1, 1, 1, 1, 1]           
+])
+b_fb =  np.array([1, 1.2, 0.8, 3.5, 0.4, 1, 2, 3, 2])
+C_fb = np.array([
+    [2, 3, 1, 0, 0, -1], 
+    [1, 2, 1, -2, 0, 0], 
+    [0, 1, 3, 0, 0, 4],   
+    [3, 0, 1, 4, 1, 0],  
+    [-1, 0, 0, 2, 3, 0]   
+])
+
+
+# Zimmermana problēma 
+
+
+Molp_problem = MolpProblem(C_fb,A_fb,b_fb)
+
+
+
+
+rez = solve_equiv(Molp_problem,   tol = 10**(-9))
+print("Risināt problēmu ar Ekvivalencēm: ", rez)
+
+rez = solve_tnorm(Molp_problem,   tol = 10**(-15), x0= None, tnorm = "luk", param = 1)
+print("Risināt problēmu ar Tnormam: ", rez)
+
+rez = solve_orders(Molp_problem, method = "sm",tol = 10**(-9) )
+print("Risināt problēmu ar Sakārtojumiem: ", rez[0])
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BakalauraDarbs
- Šeit tiek apkopoti kodi, kuri tika lietoti Bakalaura darba ietvaros
+ Šeit tiek apkopoti kodi, kuri tika lietoti Bakalaura darba ietvaros. Kods tiek sadalīts divās daļās, viena atbilst maxmin risināšanai bez ierobežojumiem. Otra MOLP prolbēmu risināšanai. 
+
+
+ ### Maxmin problēmu risināšan
+
+ ### MOLP problēmu risināšana. 
+
+    Failā main.py ir piemērs, kā tiek izsauktas visas no darbā aprakstītajām risināšanas metodēm. 
+
+    Virpirms tiek definēts pats uzdevums. Pašlaik ir iespējams risināt tikai lineārās programmēšanas uzdevumus, kuriem ierobežojumi mainīgajiem ir formā $x_{i} \geq 0$. 
+
+    
+
