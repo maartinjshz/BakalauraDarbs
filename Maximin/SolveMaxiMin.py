b7d3dbb10ff39984d7d2eadd914c1c62da0a2c7a
Mārtiņš <martins.zemlitis@gmail.com>
2024-06-25 11:34:27 +0300

    Pielabot parametrus

--- Maximin/SolveMaxiMin.py
+++ Maximin/SolveMaxiMin.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def weight_2(function_list, p = 1, fun = "Exp"):
+def weight_2(function_list,iter,  p = 1, fun = "Exp", epsilon =  10**(-6) ):
     if fun == "Exp":
         return np.exp( -  (np.abs(function_list[0][3]-function_list[1][3])  ) **(p) )
     
@@ -9,6 +9,26 @@ def weight_2(function_list, p = 1, fun = "Exp"):
     
     if fun== "Ham":
         return 1/(1 +  (np.abs(function_list[0][3]-function_list[1][3])  )**(p) )
+    
+    if fun == "E_kSubGrad":
+        if np.abs(function_list[0][3]-function_list[1][3]) <= 1/iter:
+            return 1
+        else:
+            return 0 
+        
+    if fun == "ESubGrad":
+        if np.abs(function_list[0][3]-function_list[1][3]) <= epsilon:
+            return 1
+        else:
+            return 0 
+    
+    if fun == "SubGrad":
+        if np.abs(function_list[0][3]-function_list[1][3]) ==  0:
+            return 1
+        else:
+            return 0 
+    
+
 
 def fun_val(y,function_list):
     fun_val = []
@@ -16,7 +36,8 @@ def fun_val(y,function_list):
         fun_val.append(fun[0](y))
     return min(fun_val)
 
-def direction(y, function_list,sim_fun,p):
+def direction(y, function_list,sim_fun,p,iter,epsilon):
+    AG_fun = ["Ham","Luk","Exp"]
     for idx, fun in enumerate(function_list):
         # Fun val
         fun[3]= fun[0](y)
@@ -29,10 +50,14 @@ def direction(y, function_list,sim_fun,p):
     function_list = sorted(function_list, key=lambda x: x[3])
 
     v1 = 1
-    v2 =  weight_2(function_list,  p,sim_fun)
+    v2 =  weight_2(function_list,  iter, p,sim_fun,epsilon)
 
     # Adds epsilon to denominator in case the gradient is zero. 
-    direction = v1* function_list[0][4]/ (np.linalg.norm(function_list[0][4])+ 10**(-12)) + v2* function_list[1][4]/ (np.linalg.norm(function_list[1][4])+ 10**(-12))
+    
+    if sim_fun in AG_fun:
+        direction = v1* function_list[0][4]/ (np.linalg.norm(function_list[0][4])+ 10**(-12)) + v2* function_list[1][4]/ (np.linalg.norm(function_list[1][4])+ 10**(-12))
+    else:
+        direction = v1* function_list[0][4] + v2* function_list[1][4]
     grad = function_list[0][4]
     return  direction/(v1+v2), grad
     
@@ -46,11 +71,11 @@ def line_search(y,Dir,Grad,function_list):
         return alpha
 
 
-def SolveMaximin(function_list,sim_fun,y_sol,max_iter = 1000, p = 2,tol = 10**(-9)):
-    iter = 0
+def SolveMaximin(function_list,sim_fun,y_sol,max_iter = 1000, p = 2,tol = 10**(-9),epsilon = 10**(-6)):
+    iter = 1
     while True:
         # Finds direction and gradient. Gradient is needed for Backtracking Line search algorithm. 
-        dir,grad =direction(y_sol,function_list,sim_fun,p)
+        dir,grad =direction(y_sol,function_list,sim_fun,p,iter,epsilon)
         step_size = line_search(y_sol,dir,grad,function_list)
         # returns y if step size is too small (tolerance small) or it has reached its maximum number of iterations.
         if step_size < tol or iter >= max_iter:
--- Maximin/main.py
+++ Maximin/main.py
@@ -15,27 +15,27 @@ f = lambda x :  0.49*x[0] + 0.12*x[1] + 7.93
 df_dx = lambda x :  0.49
 df_dy = lambda x :  0.12
 
-function_list.append([ f,df_dx,df_dy,1 ])
+function_list.append([ f,df_dx,df_dy ,1, np.zeros((2,))])
 
 f = lambda x :  0.3 * x[0] - 0.08*x[1]  + 8.26
 df_dx = lambda x : 0.3
 df_dy = lambda x : -0.08
-function_list.append([ f,df_dx,df_dy,1 ])
+function_list.append([ f,df_dx,df_dy ,1, np.zeros((2,))])
 
 f = lambda x :  0.39*x[0] +  0.33*x[1] + 8.34
 df_dx = lambda x : 0.39
 df_dy = lambda x : 0.33
-function_list.append([ f,df_dx,df_dy,1 ])
+function_list.append([ f,df_dx,df_dy ,1, np.zeros((2,))])
 
 f = lambda x : -0.3* x[0] + 0.016* x[1] + 8.448
 df_dx = lambda x : -0.3
 df_dy = lambda x : 0.016
-function_list.append([ f,df_dx,df_dy,1 ])
+function_list.append([ f,df_dx,df_dy ,1, np.zeros((2,))])
 
 f = lambda x : -0.191*x[0]- 0.192*x[1] + 8.469 
 df_dx = lambda x : -0.191
 df_dy = lambda x : -0.192
-function_list.append([ f,df_dx,df_dy,1 ])
+function_list.append([ f,df_dx,df_dy ,1, np.zeros((2,))])
 
 # 
 # Similarity relation can be chosen from: "Exp" / "Luk" / "Ham"
@@ -43,6 +43,6 @@ function_list.append([ f,df_dx,df_dy,1 ])
 
 # starting point is chosen randomly  
 X_start =  np.random.uniform(-4,4,2)
-X_max = SolveMaximin(function_list, "Exp" , X_start, max_iter = 250, p = 2)
+X_max = SolveMaximin(function_list, "E_kSubGrad" , X_start, max_iter = 250, p = 2)
             
 print(" Function F reaches its maximum at: ", X_max)
